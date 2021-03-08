acc9e727fd0999d9bd6a1cdbeeb5bc681172ae9f
Charlélie Bouvier <charlelie.bouvier@monext.net>
2021-03-08 12:19:23 +0100

    first commit

---
+++ src/main/java/com/monext/tdd/depenses/DepensesApplication.java
@@ -0,0 +1,13 @@
+package com.monext.tdd.depenses;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DepensesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DepensesApplication.class, args);
+	}
+
+}
